78bb3226cb152aaa2e30f27622c885c5c885e28c
SharkStewdent12 <570169@students.wits.ac.za>
2014-05-11 22:12:34 +0200

    Create bubbleSort.cc

---
+++ bubbleSort.cc
@@ -0,0 +1,16 @@
+void bubbleSort(int arr[],int length)
+{
+	int tempHolder;
+	for (int i = 0; i < length-1; i++)
+	{
+		for (int j = 0; j< (length - i-1); j++)
+		{
+			if ( arr[j] > arr[j+1] )
+			{
+				tempHolder = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tempHolder;
+			}
+		}
+	}
+}
